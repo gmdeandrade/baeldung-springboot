fd9de8b8b669324a00b930996c5300254455368e
gmdeandrade <gmdeandradee1@gmail.com>
2023-11-18 09:36:58 -0300

    first commit

---
+++ src/main/java/com/example/baeldungtutorial/BaeldungTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.baeldungtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baeldungtutorial/BaeldungTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baeldungtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
